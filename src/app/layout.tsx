fc99794aa1548570fbe6201f7aabe0c6165d4f08
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-10 20:56:28 -0600

    feat: changing the used font-family and color.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import Navbar from './components/Navbar/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ weight: '400', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} px-[8.832px]`}>
+      <body
+        className={`${montserrat.className} px-[8.832px] text-custom-green`}
+      >
         <Navbar />
         {children}
       </body>
