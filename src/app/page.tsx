f2735fcb2eff3e6b5410dfe08f87689f3f6aad8b
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-15 00:17:44 -0600

    feat: rendering stats component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import BusinessInfo from './components/BusinessInfo/BusinessInfo'
 import Hero from './components/Hero/Hero'
 import ImageSlider from './components/ImageSlider/ImageSlider'
 import Mosaic from './components/Mosaic/Mosaic'
+import Stats from './components/Stats/Stats'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <AboutCompany />
       <ImageSlider />
       <BusinessInfo />
+      <Stats />
     </main>
   )
 }
