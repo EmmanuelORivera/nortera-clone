d04b8fb1bc5e136af2fa937c95c403f3c92d980d
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-15 00:17:30 -0600

    feat: adding animation to the paragraph

--- src/app/components/AboutCompany/AboutCompany.tsx
+++ src/app/components/AboutCompany/AboutCompany.tsx
@@ -1,10 +1,17 @@
+'use client'
 import Link from 'next/link'
 import GoToArrow from '../GoToArrow/GoToArrow'
+import useIntersectionObserver from '@/app/hooks/useIntersectionObserver'
+import { MOVE_DOWN_CLASS, MOVE_UP_CLASS } from '@/app/constants/classNames'
 
 const AboutCompany = () => {
+  useIntersectionObserver(MOVE_DOWN_CLASS, (target) => {
+    target.classList.add(MOVE_UP_CLASS)
+  })
+
   return (
     <div className="pb-6 border-b border-black">
-      <p className="text-[2rem] mb-24">
+      <p className={`text-[2rem] mb-24 ${MOVE_DOWN_CLASS}`}>
         To contribute to the well-being of society through access to the
         richness of vegetables.
       </p>
---
+++ src/app/constants/classNames.ts
@@ -0,0 +1,2 @@
+export const MOVE_DOWN_CLASS = 'move-down'
+export const MOVE_UP_CLASS = 'move-up'
