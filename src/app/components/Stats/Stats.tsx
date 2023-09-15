b981045cf51d9891013034cfc90eb77da0a9a50e
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-15 15:07:01 -0600

    feat: adding fade in effect to the paragraph

--- src/app/components/Stats/Stats.tsx
+++ src/app/components/Stats/Stats.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import GoToArrow from '../GoToArrow/GoToArrow'
 import StatsData from './StatsData'
+import WrapperIntersectionObserver from '@/app/components/WrapperIntersectionObserver/WrapperIntersectionObserver'
+import { MOVE_DOWN_CLASS, MOVE_UP_CLASS } from '@/app/constants/classNames'
 
 const Stats = () => {
   return (
@@ -22,11 +24,15 @@ https://www.datocms-assets.com/85733/1681248525-betteraves-1-1-1.jpg?ixlib=js-3.
 https://www.datocms-assets.com/85733/1681248525-betteraves-1-1-1.jpg?ixlib=js-3.8.0&auto=format&w=1500 1500w,
 https://www.datocms-assets.com/85733/1681248525-betteraves-1-1-1.jpg?ixlib=js-3.8.0&auto=format&w=1800 1800w"
       />
-      <p className="text-2xl leading-7 mt-9 mb-16">
-        With facilities in Canada and the United States, Nortera processes and
-        markets over 450,000 tons of frozen and canned vegetables per year
-      </p>
-
+      <WrapperIntersectionObserver
+        targetSelector={MOVE_DOWN_CLASS}
+        classNameToAdd={MOVE_UP_CLASS}
+      >
+        <p className="text-2xl leading-7 mt-9 mb-16">
+          With facilities in Canada and the United States, Nortera processes and
+          markets over 450,000 tons of frozen and canned vegetables per year
+        </p>
+      </WrapperIntersectionObserver>
       <div className="grid grid-cols-2 gap-x-5 gap-y-16">
         <StatsData data="800" description="agricultural partners" />
         <StatsData data="120500" description="acres of land cultivated" />
