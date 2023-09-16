0bfe67aefa0be853d7ec95c978f6350f092d55fb
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-16 13:12:10 -0600

    feat: new constants for styling

--- src/app/constants/classNames.ts
+++ src/app/constants/classNames.ts
@@ -1,3 +1,5 @@
 //this are styled on global.css
 export const MOVE_DOWN_CLASS = 'move-down'
 export const MOVE_UP_CLASS = 'move-up'
+export const FINAL_ZOOM = 'final-zoom'
+export const INITIAL_ZOOM = 'initial-zoom'
