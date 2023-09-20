8a43dba718f6c6215ad89ce48fd406cbe6a2289c
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-20 15:01:41 -0600

    style: desktop styles are added

--- src/app/components/BusinessInfo/BusinessInfo.tsx
+++ src/app/components/BusinessInfo/BusinessInfo.tsx
@@ -3,25 +3,13 @@ import BusinessWrapper from './BusinessWrapper'
 
 const BusinessInfo = () => {
   return (
-    <div className="pb-16 border-b border-black">
+    <div className="pb-16 border-b border-black grid md:grid-cols-2 md:gap-4">
       <BusinessWrapper
         title="Our custom products"
         description="With over 260 products and two processing technologies, Nortera helps
         its customers stand out."
       >
-        <Image
-          className="w-full h-[50vw] object-cover"
-          alt="products"
-          width={1200}
-          height={800}
-          src="https://www.datocms-assets.com/85733/1681326677-2022-08-03_09153615_photo_by_jimmy_hamelin-1-min.jpg?ixlib=js-3.8.0&auto=format&w=100 100w,
-      https://www.datocms-assets.com/85733/1681326677-2022-08-03_09153615_photo_by_jimmy_hamelin-1-min.jpg?ixlib=js-3.8.0&auto=format&w=300 300w,
-      https://www.datocms-assets.com/85733/1681326677-2022-08-03_09153615_photo_by_jimmy_hamelin-1-min.jpg?ixlib=js-3.8.0&auto=format&w=600 600w,
-      https://www.datocms-assets.com/85733/1681326677-2022-08-03_09153615_photo_by_jimmy_hamelin-1-min.jpg?ixlib=js-3.8.0&auto=format&w=900 900w,
-      https://www.datocms-assets.com/85733/1681326677-2022-08-03_09153615_photo_by_jimmy_hamelin-1-min.jpg?ixlib=js-3.8.0&auto=format&w=1200 1200w,
-      https://www.datocms-assets.com/85733/1681326677-2022-08-03_09153615_photo_by_jimmy_hamelin-1-min.jpg?ixlib=js-3.8.0&auto=format&w=1500 1500w,
-      https://www.datocms-assets.com/85733/1681326677-2022-08-03_09153615_photo_by_jimmy_hamelin-1-min.jpg?ixlib=js-3.8.0&auto=format&w=1800 1800w"
-        />
+        <div className="h-[40vw]"></div>
       </BusinessWrapper>
 
       <BusinessWrapper
@@ -30,8 +18,8 @@ const BusinessInfo = () => {
         type="secondary"
       >
         <Image
-          className="w-full h-[50vw] object-cover"
-          style={{ objectPosition: '0 70%' }}
+          className="w-full h-[40vw] object-cover"
+          style={{ objectPosition: '0 55%' }}
           alt="brands"
           width={1200}
           height={800}
--- src/app/components/BusinessInfo/BusinessWrapper.tsx
+++ src/app/components/BusinessInfo/BusinessWrapper.tsx
@@ -22,14 +22,20 @@ const BusinessWrapper = ({
   return (
     <div
       data-testid="business-wrapper"
-      className={`${bgColor} ${className} text-white text-center hover:rounded-[35px] overflow-hidden transition-all duration-500 ease-in-out`}
+      className={`${bgColor} ${className} h-full md:relative text-white text-center hover:rounded-[35px] overflow-hidden transition-all duration-500 ease-in-out`}
     >
-      <div className="px-14 pt-14">
-        <h4 className="text-3xl font-semibold mb-4">{title}</h4>
-        <p className="text-lg leading-5 mb-3">{description}</p>
-        <GoToArrow className="mx-auto z-50 translate-y-5" />
+      <div className="px-14 pt-14 md:w-[75%] md:top-[0%] xl:top-[20%] md:left-[50%] md:-translate-x-[50%] md:absolute">
+        <h4 className="uppercase text-3xl md:text-[calc(16px+2vw)] md:leading-[3vw] font-semibold mb-4">
+          {title}
+        </h4>
+        <p className="text-lg md:text-[calc(6px+1vw)] md:leading-[2vw]  mb-3">
+          {description}
+        </p>
       </div>
-
+      <GoToArrow
+        type="big"
+        className="mx-auto z-50 translate-y-5 md:absolute md:bottom-6 md:left-[50%] md:-translate-x-[50%]"
+      />
       {children}
     </div>
   )
