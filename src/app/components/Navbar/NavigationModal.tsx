cc670960c2b660177a59bffb14b55c56d923ec1a
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-22 11:18:17 -0600

    style: adding styles upper side of the navigation

--- src/app/components/Navbar/NavigationModal.tsx
+++ src/app/components/Navbar/NavigationModal.tsx
@@ -20,11 +20,13 @@ const NavigationModal = ({ isOpen }: { isOpen: boolean }) => {
     <div
       className={`h-full w-full bg-white fixed top-0 z-40 translate-x-[100%] ${animation}`}
     >
-      <button>our food +</button>
-      <Link href="#">our approach</Link>
-      <Link href="#">jobs</Link>
-      <button>who we are +</button>
-      <Link href="#">contact us</Link>
+      <div className="absolute top-[15vh] w-full grid justify-start gap-10 text-[calc(17.6px+1vw)] uppercase font-semibold pb-12 border-b border-black">
+        <button className="text-left uppercase">our food +</button>
+        <Link href="#">our approach</Link>
+        <Link href="#">jobs</Link>
+        <button className="text-left uppercase">who we are +</button>
+        <Link href="#">contact us</Link>
+      </div>
     </div>
   )
 }
