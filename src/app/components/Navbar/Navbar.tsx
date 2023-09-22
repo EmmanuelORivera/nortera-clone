b26f37f8d4bd0af5e89cfb7f642d670efb5683f6
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-21 18:38:40 -0600

    feat: modal was added on the navbar component

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,12 +1,19 @@
+'use client'
+import { useState } from 'react'
 import BurgerIcon from './BurgerIcon'
 import Logo from './Logo'
+import NavigationModal from './NavigationModal'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <nav className="flex items-center justify-between py-6 px-[1.6667vw] md:px-[2.1333vw]">
-      <Logo />
-      <BurgerIcon />
-    </nav>
+    <>
+      <nav className="flex items-center relative bg-transparent justify-between py-6 px-[1.6667vw] md:px-[2.1333vw] z-50">
+        <Logo />
+        <BurgerIcon isOpen={isOpen} setIsOpen={setIsOpen} />
+      </nav>
+      <NavigationModal isOpen={isOpen} />
+    </>
   )
 }
 
