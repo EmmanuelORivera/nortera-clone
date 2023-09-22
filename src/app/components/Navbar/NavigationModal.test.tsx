4309794d6e9110bbafe1bbf675c8fed39c05d697
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-22 10:59:04 -0600

    test: about elements that should exists

--- src/app/components/Navbar/NavigationModal.test.tsx
+++ src/app/components/Navbar/NavigationModal.test.tsx
@@ -5,4 +5,42 @@ describe('NavigationModal', () => {
   it('should render the component without problems', () => {
     render(<NavigationModal isOpen={false} />)
   })
+
+  it('should have a button with the "our food +" text', () => {
+    render(<NavigationModal isOpen={true} />)
+
+    const buttonElement = screen.getByRole('button', { name: 'our food +' })
+    expect(buttonElement).toBeInTheDocument()
+  })
+
+  it('should have a link with the text "our approach" and has a correct href proprerty', () => {
+    render(<NavigationModal isOpen={true} />)
+
+    const linkElement = screen.getByRole('link', { name: 'our approach' })
+    expect(linkElement).toBeInTheDocument()
+    expect(linkElement).toHaveAttribute('href', '#')
+  })
+
+  it('should have a link with the text "jobs" and has a correct href proprerty', () => {
+    render(<NavigationModal isOpen={true} />)
+
+    const linkElement = screen.getByRole('link', { name: 'jobs' })
+    expect(linkElement).toBeInTheDocument()
+    expect(linkElement).toHaveAttribute('href', '#')
+  })
+
+  it('should have a button with the "who we are +" text', () => {
+    render(<NavigationModal isOpen={true} />)
+
+    const buttonElement = screen.getByRole('button', { name: 'who we are +' })
+    expect(buttonElement).toBeInTheDocument()
+  })
+
+  it('should have a link with the text "contact us" and has a correct href proprerty', () => {
+    render(<NavigationModal isOpen={true} />)
+
+    const linkElement = screen.getByRole('link', { name: 'contact us' })
+    expect(linkElement).toBeInTheDocument()
+    expect(linkElement).toHaveAttribute('href', '#')
+  })
 })
