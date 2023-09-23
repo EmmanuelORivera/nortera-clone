f75fee31e71a05978e2b0f0500c715b7ee332986
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-22 18:03:28 -0600

    feat: adding scroll effect to the navbar

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,19 +1,52 @@
 'use client'
-import { useState } from 'react'
+import { useLayoutEffect, useRef, useState } from 'react'
 import BurgerIcon from './BurgerIcon'
 import Logo from './Logo'
 import NavigationModal from './NavigationModal'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [isBackgroundWhite, setIsBackgroundWhite] = useState(false)
   const [navigationModalAnimation, setNavigationModalAnimation] = useState('')
+  const navbar = useRef<HTMLElement | null>(null)
+  const backgroundColor = isBackgroundWhite ? 'bg-white' : 'bg-transparent'
+
+  useLayoutEffect(() => {
+    let lastScroll = 0
+
+    const handleScroll = () => {
+      const currentScroll = window.scrollY
+
+      if (navbar.current)
+        if (currentScroll <= 0) {
+          navbar.current.classList.remove('animate-scrollUp')
+          setIsBackgroundWhite(false)
+        } else if (currentScroll > lastScroll) {
+          navbar.current.classList.add('scrollUp')
+          setIsBackgroundWhite(true)
+        } else if (currentScroll < lastScroll) {
+          navbar.current.classList.remove('scrollUp')
+          setIsBackgroundWhite(true)
+        }
+      lastScroll = currentScroll
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <>
-      <header className="fixed top-0 w-full z-50">
-        <nav className="flex items-center relative bg-transparent justify-between py-6 px-[1.6667vw] md:px-[2.1333vw] z-50">
+      <header ref={navbar} className={`fixed top-0 w-full z-50 transition-all`}>
+        <nav
+          className={`${backgroundColor} transition-all flex items-center relative justify-between py-6 px-[1.6667vw] md:px-[2.1333vw] z-50`}
+        >
           <Logo />
           <BurgerIcon
             isOpen={isOpen}
+            isBackgroundWhite={isBackgroundWhite}
             setIsOpen={setIsOpen}
             setNavigationModalAnimation={setNavigationModalAnimation}
           />
