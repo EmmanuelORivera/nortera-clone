4c207f51e5b31720208c1635e32ce88dccc44687
Emmanuel Ochoa <eorivera95@gmail.com>
2023-09-25 12:37:01 -0600

    feat: desktop footer view

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,24 +2,26 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className="mt-14">
-      <div className="flex text-sm gap-[2.13vw] mb-16 justify-between">
+    <footer className="py-28 lg:flex lg:justify-between">
+      <div className="flex text-sm lg:text-[1vw] gap-[2.13vw] mb-16 justify-between lg:flex-col">
         <div className="flex-1">
-          <p className="uppercase font-semibold ">Canada</p>
+          <p className="uppercase lg:text-[1.1vw] font-semibold ">Canada</p>
           <p>140, rue de l&apos;Équinoxe</p>
           <p>Brossard, QC J4Z 0R4</p>
         </div>
         <div className="flex-1">
-          <p className="uppercase font-semibold ">United States</p>
+          <p className="uppercase lg:text-[1.1vw] font-semibold ">
+            United States
+          </p>
           <p>100 Corporate Woods, Suite 210</p>
           <p>Rochester, NY 14623</p>
         </div>
       </div>
-      <div className="flex text-sm gap-[2.13vw] justify-between">
+      <div className="flex text-sm lg:text-[1vw] gap-[2.13vw] justify-between">
         {/* left */}
-        <div className="flex flex-col gap-[10.67vw] flex-1">
+        <div className="flex flex-col gap-[1.5vw] flex-1">
           <div className="grid gap-2">
-            <p className="uppercase font-semibold ">Our food</p>
+            <p className="uppercase lg:text-[1.1vw] font-semibold ">Our food</p>
             <Link href="#" className="block">
               Our Custom Products
             </Link>
@@ -28,45 +30,62 @@ const Footer = () => {
             </Link>
           </div>
           <div>
-            <Link href="#" className="uppercase font-semibold ">
+            <Link href="#" className="uppercase lg:text-[1.1vw] font-semibold ">
               Our Approach
             </Link>
           </div>
           <div>
-            <Link href="#" className="uppercase font-semibold ">
+            <Link href="#" className="uppercase lg:text-[1.1vw] font-semibold ">
               Contact Us
             </Link>
           </div>
         </div>
         {/* right */}
-        <div className="flex flex-col gap-[10.67vw] flex-1">
-          <div className="grid gap-2">
-            <p className="uppercase font-semibold ">Jobs</p>
-            <Link href="#">Working at Nortera</Link>
-            <Link href="#">Our Open Positions</Link>
+        <div className="flex flex-col lg:flex-row lg:gap-[6.25vw] lg:mr-[min(88px,6.1111111111vw)] gap-[1.5vw] flex-1">
+          <div>
+            <div className="grid gap-2 mb-2">
+              <p className="uppercase lg:text-[1.1vw] font-semibold ">Jobs</p>
+              <Link href="#">Working at Nortera</Link>
+              <Link href="#">Our Open Positions</Link>
+            </div>
+
+            <div className="grid gap-2">
+              <p className=" uppercase lg:leading-8 lg:text-[1.1vw] font-semibold lg:w-[200px]">
+                Who we are
+              </p>
+              <Link href="#">About Us</Link>
+              <Link href="#">Our Commitments</Link>
+            </div>
           </div>
+          <div className="grid gap-4 lg:leading-8">
+            <Link
+              href="#"
+              className="uppercase lg:text-[1.1vw] font-semibold  block"
+            >
+              Press Relations
+            </Link>
 
-          <div className="grid gap-2">
-            <p className=" uppercase font-semibold ">Who we are</p>
-            <Link href="#">About Us</Link>
-            <Link href="#">Our Commitments</Link>
+            <Link
+              href="#"
+              className="uppercase lg:text-[1.1vw] font-semibold  block"
+            >
+              FAQ
+            </Link>
+
+            <Link
+              href="#"
+              className="uppercase lg:text-[1.1vw] font-semibold  block"
+            >
+              Partners
+            </Link>
+
+            <Link
+              href="#"
+              className="uppercase lg:text-[1.1vw] font-semibold  block"
+            >
+              Conditions
+            </Link>
           </div>
-
-          <Link href="#" className="uppercase font-semibold  block">
-            Press Relations
-          </Link>
-
-          <Link href="#" className="uppercase font-semibold  block">
-            FAQ
-          </Link>
-
-          <Link href="#" className="uppercase font-semibold  block">
-            Partners
-          </Link>
-
-          <Link href="#" className="uppercase font-semibold  block">
-            Conditions
-          </Link>
         </div>
       </div>
     </footer>
